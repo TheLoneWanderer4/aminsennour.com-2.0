854bddc794337e5ded4d52e032f373f37bcfe88c
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-02 23:14:31 -0500

    Update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,44 +2,48 @@ import React from "react";
 import "./Home.css";
 
 const Home = props => (
-  <div>
-    <div className="welcome-section">
-      <section className="intro-section card shadow-sm content">
-        <h1>Hi, I'm Amin Sennour.</h1>
-        <p>{"{developer}"}</p>
-        <p>{"{CS major at the UofA}"}</p>
-      </section>
-
-      <div className="content card shadow education-content">
-        <h1>Education</h1>
-        <h4>University of Arizona</h4>
-        <p>
-          <b>Projected Graduation: </b> 2022
-        </p>
-        <p>
-          <b> College of Science </b>
-        </p>
-        <li>Major: Computer Science</li>
-        <p>
-          <b>College of Science</b>
-        </p>
-        <li>Major: Mathematics</li>
-        <p></p>
-        <p>
-          <b>GPA: </b> 3.71
-        </p>
+  <div className="">
+    <div className="row m-0 p-0 align-items-center">
+      <div className="col-md m-2">
+        <div className="card p-2">
+          {" "}
+          <h1>Hi, I'm Amin Sennour.</h1>
+          <p>{"{developer}"}</p>
+          <p>{"{CS major at the UofA}"}</p>
+        </div>
+      </div>
+      <div className="col-md m-2">
+        <div className="card p-2">
+          {" "}
+          <h1>University of Arizona</h1>
+          <p>
+            <b>Projected Graduation: </b> 2022
+          </p>
+          <p>
+            <b> College of Science </b>
+          </p>
+          <li>Major: Computer Science</li>
+          <p>
+            <b>College of Science</b>
+          </p>
+          <li>Major: Mathematics</li>
+          <p></p>
+          <p>
+            <b>GPA: </b> 3.71
+          </p>
+        </div>
       </div>
     </div>
-    <div className="card ">
-      <ul className="list-group list-group-flush">
-        {props.projectsList.map(item => (
-          <li key={item.label}>
-            <a className="list-group-item" href={"/#/" + item.label}>
-              {item.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="">
+      <div className="card m-4">
+        <ul className="list-group list-group-flush">
+          {props.projectsList.map(item => (
+            <li key={item.label} className="list-group-item">
+              <a href={"/#/" + item.label}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   </div>
 );
