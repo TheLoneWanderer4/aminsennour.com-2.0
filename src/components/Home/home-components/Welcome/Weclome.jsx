b29357f94281626f6f4bb6aea6173de67ec62514
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-05 16:22:31 -0500

    Update

--- src/components/Home/home-components/Welcome/Weclome.jsx
+++ src/components/Home/home-components/Welcome/Weclome.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Welcome.css";
 
 const Welcome = () => (
-  <div className="p-4 home-card intro-section d-flex flex-column justify-content-center">
-    <div className="bg-light card p-2">
-      <div className="card-body">
-        <h1 className="card-title">Hi, I'm Amin Sennour.</h1>
-        <p>{"{developer}"}</p>
-        <p>{"{CS major at the UofA}"}</p>
+  <div className="p-4 home-card intro-section">
+    <div className="intro-card-wrapper d-flex flex-column justify-content-center">
+      <div className="bg-light card p-2">
+        <div className="card-body">
+          <h1 className="card-title">Hi, I'm Amin Sennour.</h1>
+          <p>{"{developer}"}</p>
+          <p>{"{CS major at the UofA}"}</p>
+        </div>
       </div>
     </div>
   </div>
--- src/components/ProjectPage/ProjectPage.jsx
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -4,12 +4,14 @@ import "./ProjectPage.css";
 
 const ProjectPage = props => (
   <div>
-    <div className="p-4 project-card-wrapper d-flex flex-column justify-content-center">
-      <div className="card project-card shadow-sm">
-        <div className="card-body text-dark bg-light border-bottom">
-          <h3 className="card-title">{props.projectsJSON.label}</h3>
+    <div className="p-4 project-wrapper">
+      <div className="project-card-wrapper d-flex flex-column justify-content-center">
+        <div className="card project-card shadow-sm">
+          <div className="card-body text-dark bg-light border-bottom">
+            <h3 className="card-title">{props.projectsJSON.label}</h3>
+          </div>
+          <div className="card-body">{props.projectsJSON.desc}</div>
         </div>
-        <div className="card-body">{props.projectsJSON.desc}</div>
       </div>
     </div>
     <ProjectList projects={props.projectsJSON.projects} />
