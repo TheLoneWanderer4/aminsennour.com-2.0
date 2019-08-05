58f68bb55cc62e8242bf4fe6681350e8825b05e2
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-05 16:10:42 -0500

    Update

--- src/components/Home/home-components/Welcome/Weclome.jsx
+++ src/components/Home/home-components/Welcome/Weclome.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Welcome.css";
 
 const Welcome = () => (
-  <div className="p-4 home-card">
-    <div className="intro-section bg-light card p-2">
+  <div className="p-4 home-card intro-section d-flex flex-column justify-content-center">
+    <div className="bg-light card p-2">
       <div className="card-body">
         <h1 className="card-title">Hi, I'm Amin Sennour.</h1>
         <p>{"{developer}"}</p>
--- src/components/ProjectPage/ProjectPage.jsx
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -4,7 +4,7 @@ import "./ProjectPage.css";
 
 const ProjectPage = props => (
   <div>
-    <div className="p-4 project-card-wrapper">
+    <div className="p-4 project-card-wrapper d-flex flex-column justify-content-center">
       <div className="card project-card shadow-sm">
         <div className="card-body text-dark bg-light border-bottom">
           <h3 className="card-title">{props.projectsJSON.label}</h3>
