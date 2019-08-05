99ee50cd33569ddb0a48a98c4d6db0c8f2bc315e
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-05 16:15:45 -0500

    Update, beta 2.0

--- src/components/Home/home-components/Education/Education.jsx
+++ src/components/Home/home-components/Education/Education.jsx
@@ -2,27 +2,15 @@ import React from "react";
 import "./Education.css";
 
 const Education = () => (
-  <div className="p-4 home-card">
-    <div className="card bg-light">
-      <div className="card-body">
-        <h1 className="card-title">Education</h1>
-        <h5 className="card-title">University of Arizona</h5>
-        <p>
-          <b>Projected Graduation: </b> 2022
-        </p>
-        <p>
-          <b> College of Science </b>
-        </p>
-        <li>Major: Computer Science</li>
-        <p>
-          <b>College of Science</b>
-        </p>
-        <li>Major: Mathematics</li>
-        <p></p>
-        <p>
-          <b>GPA: </b> 3.71
-        </p>
-      </div>
+  <div className="p-4 home-card text-center">
+    <div className="card bg-light p-2">
+      <h1 className="card-title">Education</h1>
+      <h5 className="card-title">University of Arizona</h5>
+      <ul className="list-group list-group-flush">
+        <b> College of Science : </b>
+        <li className="list-group-item">Computer Science</li>
+        <li className="list-group-item">Mathematics</li>
+      </ul>
     </div>
   </div>
 );
