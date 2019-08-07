3b861f4011442b6b15d8f1806fe0fc5eb1f16e79
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-07 12:04:03 -0500

    update

--- src/components/Home/home-components/ContactMe/ContactMe.jsx
+++ src/components/Home/home-components/ContactMe/ContactMe.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import "./ContactMe.css";
-import contacts from "./contacts.json";
 
-console.log(contacts);
+class ContactMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { contacts: [] };
+  }
 
-const ContactMe = props => (
-  <div className="home-card p-4">
-    <div className="card shadow-lg">
-      <div className="card-body border-bottom bg-light text-center">
-        <h3 className="card-title m-0">Contact Me</h3>
-      </div>
-      <div className="card-body d-flex flex-column align-items-center">
-        <div>
-          {contacts.map(contact => (
-            <li>
-              <a className="" href={contact.url}>
-                {contact.label}{" "}
-              </a>
-            </li>
-          ))}
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/TheLoneWanderer4/aminsennour.com-2.0/master/src/components/Home/home-components/ContactMe/contacts.json"
+    )
+      .then(response => response.json())
+      .then(contacts => {
+        this.setState({ contacts });
+      });
+  }
+
+  render() {
+    return (
+      <div className="home-card p-4">
+        <div className="card shadow-lg">
+          <div className="card-body border-bottom bg-light text-center">
+            <h3 className="card-title m-0">Contact Me</h3>
+          </div>
+          <div className="card-body d-flex flex-column align-items-center">
+            <div>
+              {this.state.contacts.map(contact => (
+                <li>
+                  <a className="" href={contact.url}>
+                    {contact.label}{" "}
+                  </a>
+                </li>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default ContactMe;
--- src/components/Home/home-components/Welcome/Weclome.jsx
+++ src/components/Home/home-components/Welcome/Weclome.jsx
@@ -4,7 +4,7 @@ import "./Welcome.css";
 const Welcome = () => (
   <div className="p-4 home-card intro-section">
     <div className="intro-card-wrapper d-flex flex-column justify-content-center">
-      <div className="card p-2 shadow-lg">
+      <div className="card shadow-lg">
         <div className="card-body border-bottom bg-light">
           <h1 className="card-title m-0">Hi, I'm Amin Sennour.</h1>
         </div>
