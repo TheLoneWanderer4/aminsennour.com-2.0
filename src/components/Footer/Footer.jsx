fa4185b166d2852f0c6304c9221526f2367dfe75
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-08 17:38:04 -0500

    Fixed a stupid bug

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = ({}) => (
-  <footer id="footer" className="bg-dark text-light text-center p-4">
-    <div className="copyright">Made from Dallas TX and Tucson AZ</div>
+  <footer className="bg-dark text-light text-center p-4">
+    Made from Dallas TX and Tucson AZ
   </footer>
 );
 
